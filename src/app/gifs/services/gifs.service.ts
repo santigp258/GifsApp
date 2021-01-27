9a9b7c54a5ef2a31ee42f6b959aeae65d77343de
Santiago <santigp258@gmail.com>
2021-01-27 15:56:01 -0500

    try http client

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,22 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  private apiKey: string = 'cHmxZ109D6hGo5rltOLpXg8XGOgOgGOS';
   private _history: string[] = [];
 
   get history() {
     return [...this._history]; //spreed operator, rompe la referencia
   }
 
-  searchGifs(query: string) {
-    query = query.trim().toLowerCase();
+  constructor(private http: HttpClient) {}
+
+  searchGifs(query: string = '') {
+    query = query.trim().toLocaleLowerCase();
     if (!this._history.includes(query)) {
       this._history.unshift(query); //insert al inicio
       this._history = this._history.splice(0, 10); //limitar el numero del historial
     }
+
+    this.http
+      .get(
+        'https://api.giphy.com/v1/stickers/search?api_key=cHmxZ109D6hGo5rltOLpXg8XGOgOgGOS&q=hola&limit=10'
+      )
+      .subscribe((resp: any) => {
+        console.log(resp.data);
+      }); //se ejecuta cuando tenga la resolución del get
     console.log(this._history);
   }
-  constructor() {}
 }
