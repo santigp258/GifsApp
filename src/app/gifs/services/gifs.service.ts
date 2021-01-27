180ae9949bce544431bc787262df125cf46cdc63
Santiago <santigp258@gmail.com>
2021-01-27 16:35:02 -0500

    add SearchGifsResponse

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsReponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ export class GifsService {
   private _history: string[] = [];
   
   //TODO cambiar any por su tipo correspondiente
-  public results: any[] = [];
+  public results: Gif[] = [];
 
   get history() {
     return [...this._history]; //spreed operator, rompe la referencia
@@ -25,12 +26,12 @@ export class GifsService {
     }
 
     this.http
-      .get(
-        `https://api.giphy.com/v1/stickers/search?api_key=cHmxZ109D6hGo5rltOLpXg8XGOgOgGOS&q=${query}&limit=10`
+      .get<SearchGifsReponse>(
+        `https://api.giphy.com/v1/stickers/search?api_key=${this.apiKey}&q=${query}&limit=10`
       )
-      .subscribe((resp: any) => {
+      .subscribe((resp) => {
+        console.log(resp.data);
         this.results = resp.data;
       }); //se ejecuta cuando tenga la resolución del get
-    console.log(this._history);
   }
 }
